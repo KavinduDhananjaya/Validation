0dea353f08733997007a27b46c32bf63615b13e3
Kavindu <dhananjayaa1998@gmail.com>
2019-06-17 14:11:37 +0530

    second

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: User
+ * Date: 6/17/2019
+ * Time: 9:31 AM
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Customer</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+
+</head>
+<body>
+
+
+
+    <form style="width: 90%;margin-left: 2%">
+        <div class="form-group">
+            <label for="exampleInputEmail1">CID</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Name</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+
+
+
+
+
+</body>
+</html>
+
